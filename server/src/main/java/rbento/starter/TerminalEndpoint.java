021092013f5380e8156182e225a0f8a7d4635b48
Rodrigo Bento <rodrigo.bento@gmail.com>
2023-08-05 15:57:55 -0400

    Add simplest Terminal gRPC service implementation

---
+++ server/src/main/java/rbento/starter/TerminalEndpoint.java
@@ -0,0 +1,34 @@
+
+/* Copyright (c) 2023 Rodrigo Bento */
+
+package rbento.starter;
+
+import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import org.lognet.springboot.grpc.GRpcService;
+import rbento.api.v1.CommandRequest;
+import rbento.api.v1.CommandResponse;
+import rbento.api.v1.TerminalGrpc;
+
+@Slf4j
+@GRpcService
+public class TerminalEndpoint extends TerminalGrpc.TerminalImplBase {
+
+    @Override
+    public void executeCommand(CommandRequest request, StreamObserver<CommandResponse> responseObserver) {
+        log.info("executeCommand[request={}]", request);
+        if ("ls".equalsIgnoreCase(request.getStatement())) {
+            CommandResponse response = CommandResponse.newBuilder()
+                    .setCode(200)
+                    .setResult("README.md")
+                    .build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } else {
+            responseObserver.onError(Status.INVALID_ARGUMENT
+                    .withDescription("Bad command or file name")
+                    .asRuntimeException());
+        }
+    }
+}
